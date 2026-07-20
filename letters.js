dcb0dccd48a0b57165703a1beaeb6c5d95bc4f91
Eter <156882416+FFounddd@users.noreply.github.com>
2026-07-19 21:58:06 -0700

    Create apology letter template in letters.js
    Added a letter template for an apology to Medea.

---
+++ letters.js
@@ -0,0 +1,70 @@
+// ======================================================
+// Hidden Letters
+// letters.js
+// ======================================================
+
+const Letters = {
+
+    apology: `
+
+To Medea,
+
+I don't know if this is the first time I've written this anymore.
+
+It might be the hundredth.
+
+It might be the thousandth.
+
+I can't remember anymore...
+
+They told me I wasn't writing this because I truly felt remorse for my actions.
+
+They said I was writing it so I wouldn't forget.
+
+Every morning they reminded me.
+
+Every examination.
+
+Every mission.
+
+Every time they locked the suit around me.
+
+Every time they looked through the glass.
+
+They'd ask me the same question.
+
+"Why is she dead?"
+
+And before I could answer...
+
+They answered it for me.
+
+"Because of you."
+
+They told me your blood was on my hands.
+
+They told me that everything they had me do was a way to repay the things I'd done to you.
+
+They told me I should be grateful that you had even stuck around me to begin with.
+
+That if I ever stopped working...
+
+Stopped fighting...
+
+Stopped obeying...
+
+Then your death would've meant nothing.
+
+At first I fought them...
+
+...Paste the rest of your letter here...
+
+I just hope...
+
+...wherever you are...
+
+...you still do.
+
+`
+
+};
